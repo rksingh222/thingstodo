8396bc75708303d4a90df95765f7c214418c28aa
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-01-14 15:20:45 +1100

    Create thingstodoinaustralia.js

---
+++ thingstodoinaustralia.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            height: 100%;
+            width: 100%;
+            background: #f5f6fa;
+        }
+
+        div {
+            color: #67727e;
+            font-size: 20px;
+            padding-left: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <div>
+      1. Buy a leg stabler if you can
+      2. Buy a rechargable lamp
+      3. Buy a fan (rechargable fan)
+      4. Buy a tent adjustible pole  
+    </div>
+</body>
+
+</html>
